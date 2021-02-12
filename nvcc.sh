8187034abe82819d8c0e23f8e30ac85ed752880c
developer-onizuka <his.onioni@gmail.com>
2021-02-12 16:13:34 +0900

    2021-02-12

---
+++ nvcc.sh
@@ -0,0 +1,1 @@
+nvcc -I /usr/local/cuda/include/  -I /usr/local/cuda/targets/x86_64-linux/lib/ strrev_gds.cu -o strrev_gds.co -L /usr/local/cuda/targets/x86_64-linux/lib/ -lcufile -L /usr/local/cuda/lib64/ -lcuda -L   -Bstatic -L /usr/local/cuda/lib64/ -lcudart_static -lrt -lpthread -ldl -lcrypto -lssl
